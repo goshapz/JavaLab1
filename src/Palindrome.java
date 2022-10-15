6e4b09a99a85f144c72c3df6813628c15e5290a8
Gosha <gosha1253@mail.ru>
2022-10-15 21:45:19 +0300

    changes 15/10

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Palindrome {
-}
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a word: ");
+        String word = in.nextLine();
+        String reversed = reverseString(word);
+        System.out.println("Reversed word: " + reversed);
+        if (isPalindrome(word))
+            System.out.println("This word is a palindrome.");
+        else System.out.println("This word isn`t a palindrome.");
+    }
+    public static String reverseString(String s){
+        int length = s.length();
+        String r = "";
+        while(length > 0){
+            r = r + s.charAt(length - 1);
+            length--;
+        }
+        return r;
+    }
+    public static boolean isPalindrome(String s){
+        return reverseString(s).equals(s);
+    }
+
+
+
+    }
--- src/Primes.java
+++ src/Primes.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Primes {
+public class Primes {
+    public static void main(String[] args) {
+        System.out.println("Prime numbers in [1; 100]:");
+        int i = 2;
+        while (i<=100){
+            if (isPrime(i)){
+                System.out.print(i + " ");}
+            i = i + 1;
+    }
+    }
+
+    public static boolean isPrime(int n) {
+        int m = n;
+        while (m > 2){
+            if (n%(m-1)==0)
+                return false;
+            else
+                m = m - 1;
+        }
+        return true;
+    }
 }
